aa6e24019f291b6a2c7369f517c1da994b9446f4
Álvaro Madrid Lara <alvaro.madrid.lara.contractor@bbva.com>
2019-05-05 14:41:44 +0200

    feat(appointmentController): tdd

---
+++ test/unit/appointmentController.test.js
@@ -0,0 +1,99 @@
+const appointmentController = require('../../src/controllers/appointmentController');
+const database = require('../../src/database/index');
+
+describe('appointmentController', () => {
+  describe('when newAppointment is called', () => {
+    const req = {
+      name: 'johny',
+      lastname: 'reland',
+      address: 'vanlose allé 123',
+      date: '04-05-2019',
+      reason: 'oh my knee!',
+    };
+    const res = {
+      status: jest.fn(),
+      json: jest.fn(),
+    };
+
+    test('save appointment data in database', () => {
+      database.saveAppointment = jest.fn((request, callback) => {
+        callback(null, {
+          id: '1234b',
+          name: 'johny',
+          lastname: 'reland',
+          address: 'vanlose allé 123',
+          date: '04-05-2019',
+          reason: 'oh my knee!',
+        });
+      });
+
+      appointmentController.newAppointment(req, res);
+      return expect(database.saveAppointment.mock.calls[0][0]).toBe(req);
+    });
+    test('responds with status 200', () => {
+      database.saveAppointment = jest.fn((request, callback) => {
+        callback(null, {
+          id: '1234b',
+          name: 'johny',
+          lastname: 'reland',
+          address: 'vanlose allé 123',
+          date: '04-05-2019',
+          reason: 'oh my knee!',
+        });
+      });
+
+      appointmentController.newAppointment(req, res);
+      return expect(res.status).toBeCalledWith(200);
+    });
+    test('responds the correct body', () => {
+      database.saveAppointment = jest.fn((request, callback) => {
+        callback(null, {
+          id: '1234b',
+          name: 'johny',
+          lastname: 'reland',
+          address: 'vanlose allé 123',
+          date: '04-05-2019',
+          reason: 'oh my knee!',
+        });
+      });
+
+      const expectedResponseBody = {
+        status: 'success',
+        message: 'appointment correctly saved',
+        data: {
+          id: '1234b',
+          name: 'johny',
+          lastname: 'reland',
+          address: 'vanlose allé 123',
+          date: '04-05-2019',
+          reason: 'oh my knee!',
+        },
+      };
+      appointmentController.newAppointment(req, res);
+      return expect(res.json).toBeCalledWith(expectedResponseBody);
+    });
+
+    describe('and an error ocurs saving data in database', () => {
+      test('responds with 500 status', () => {
+        database.saveAppointment = jest.fn((request, callback) => {
+          callback('error doing things in database', {});
+        });
+
+        appointmentController.newAppointment(req, res);
+        return expect(res.status).toBeCalledWith(500);
+      });
+      test('responds with the correct error body', () => {
+        database.saveAppointment.mockRestore();
+        database.saveAppointment = jest.fn((request, callback) => {
+          callback('error doing things in database', {});
+        });
+        const expectedResponseBody = {
+          status: 'error',
+          message: 'error in database',
+          data: 'error doing things in database',
+        };
+        expect(res.json).toBeCalledWith(expectedResponseBody);
+      });
+    });
+  });
+});
