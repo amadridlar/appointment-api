bb8f57ed45b9fbb2a09d10af80b810ef21354ddb
Álvaro Madrid Lara <alvaro.madrid.lara.contractor@bbva.com>
2019-05-12 13:51:07 +0200

    feat(main): database connection configuration

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ const colors = require('colors');
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const rootRoutes = require('../src/routes/rootRoute');
 const appointmentRoot = require('../src/routes/appointmentRoot');
 
@@ -15,6 +16,9 @@ app.use(bodyParser.json());
 // ROUTES
 app.use('/', rootRoutes);
 app.use('/appointments', appointmentRoot);
+
+// Database connection
+mongoose.connect('mongodb://localhost:27017/mongodb', {useNewUrlParser: true});
 
 const server = app.listen(serverPort, () => {
   console.log(colors.bgGreen(`Server is up and running on port ${serverPort}`));
